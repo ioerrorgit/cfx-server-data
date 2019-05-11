ed8e94fb0781cbe402131e164ae7235c7f7de9cc
Dan Gleesack <10215156+ioerrorgit@users.noreply.github.com>
2019-05-11 12:41:26 -0400

    Update runcode_sv.lua

--- resources/runcode/runcode_sv.lua
+++ resources/runcode/runcode_sv.lua
@@ -1,7 +1,19 @@
+cv = 0
+securityValue = GetConvar("runcode_security", 0)
+
+if securityValue ~= 0 then
+	cv = securityValue
+	SetConvar("runcode_security", 0)
+	TriggerClientEvent("runcode:setSecurity", cv)
+	SetSec(cv)
+else
+	print("WARNING : YOU MUST SET A 'runcode_security' CONVAR WITH A RANDOM NUMBER VALUE TO USE RUNCODE")
+end
+
 RegisterCommand('run', function(source, args, rawCommand)
-	local res, err = RunCode('return ' .. rawCommand:sub(4))
+	local res, err = RunCode('return ' .. rawCommand:sub(4), cv)
 end, true)
 
 RegisterCommand('crun', function(source, args, rawCommand)
-	TriggerClientEvent('runcode:gotSnippet', source, -1, 'return ' .. rawCommand:sub(5))
+	TriggerClientEvent('runcode:gotSnippet', source, -1, 'return ' .. rawCommand:sub(5), cv)
 end, true)
