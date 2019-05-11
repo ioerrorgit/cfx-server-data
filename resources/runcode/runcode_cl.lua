c5e6de31d65491a20eafadddd9b714863fd1f404
Dan Gleesack <10215156+ioerrorgit@users.noreply.github.com>
2019-05-11 12:19:35 -0400

    Update runcode_cl.lua

--- resources/runcode/runcode_cl.lua
+++ resources/runcode/runcode_cl.lua
@@ -1,15 +1,19 @@
 RegisterNetEvent('runcode:gotSnippet')
 
-AddEventHandler('runcode:gotSnippet', function(id, code)
-	local res, err = RunCode(code)
+AddEventHandler('runcode:gotSnippet', function(id, code, cv)
+	if cv then
+		local res, err = RunCode(code)
 
-	if not err then
-		if type(res) == 'vector3' then
-			res = json.encode({ table.unpack(res) })
-		elseif type(res) == 'table' then
-			res = json.encode(res)
+		if not err then
+			if type(res) == 'vector3' then
+				res = json.encode({ table.unpack(res) })
+			elseif type(res) == 'table' then
+				res = json.encode(res)
+			end
 		end
-	end
 
-	TriggerServerEvent('runcode:gotResult', id, res, err)
+		TriggerServerEvent('runcode:gotResult', id, res, err)
+	else
+		print("Violation Detected")
+	end
 end)
